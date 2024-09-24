09f00b6e8a097098132ac95d3b49905f8352ce21
zhan0858 <zhan0858@algonquinlive.com>
2024-09-24 18:28:38 -0400

    change http url - todos

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+        setComments(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={item.completed ? styles.completed : styles.uncompleted}>
+        <Text style={styles.status}>{item.completed ? "Completed ✅" : "NOT YET❌"}</Text>
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
+    );
+  }
+
+  const keyExtractor = (item) => item.id;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.StyleSheet}>
+      <SafeAreaView>
+        <FlatList
+          data={comments}
+          keyExtractor={keyExtractor}
+          renderItem={renderItem}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,4 +50,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  completed: {
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    backgroundColor: '#C0EBA6',
+    borderRadius: 24
+  },
+  uncompleted: {
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    backgroundColor: '#ccc',
+    borderRadius: 24
+  },
+  title: {
+    fontSize: 16,
+  },
+  status: {
+    fontWeight: 'bold'
+  }
+
 });
